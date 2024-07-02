d079785bf6ae0a3290ec38474a5f36c7fe2219a6
shohokuno10 <69888851+shohokuno10@users.noreply.github.com>
2024-07-02 14:45:10 +0800

    Update 20240612-thunder_UI.py

--- 20240612-thunder_UI.py
+++ 20240612-thunder_UI.py
@@ -13,7 +13,7 @@ import sys
 import io
 
 # 安裝 pandas_ta
-#subprocess.check_call([sys.executable, "-m", "pip", "install", "pandas_ta"])
+subprocess.check_call([sys.executable, "-m", "pip", "install", "pandas_ta"])
 import pandas_ta as ta
 
 # Function to download file from GitHub
